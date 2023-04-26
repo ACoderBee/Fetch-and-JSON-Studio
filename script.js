cb1c0f345a9dc1e7d7875643850af426f4896d2a
Andrea <93172228+Arionly@users.noreply.github.com>
2023-04-26 17:49:18 -0500

    What the heck no bonus

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // TODO: add code here
+window.addEventListener('load', function () {
+   const container = document.getElementById("container");
+
+   fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function (response) {
+    response.json().then (function(jsonData){
+        
+        console.log(jsonData);
+
+        jsonData.sort(function(a, b){
+            return a.hoursInSpace < b.hoursInSpace ? 1:-1;
+        });
+        for(let i = 0; i<jsonData.length; i++){
+            let astronaut = jsonData[i];
+            container.innerHTML +=
+        `<div class="astronaut">
+        <div class="bio">
+           <h3>${astronaut.firstName} ${astronaut.lastName})</h3>
+           <ul>
+              <li>Hours in space: ${astronaut.hoursInSpace}</li>
+              <li>Active: ${astronaut.active}</li>
+              <li>Skills: ${astronaut.skills.join(", ")}</li>
+           </ul>
+        </div>
+        <img class="avatar" src="${astronaut.picture}">
+     </div>
+        `
+        }
+         
+    });
+    
+   })
+});
